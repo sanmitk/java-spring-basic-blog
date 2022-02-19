fc276d193dc2065efbbc58cc564ba23ace46c630
Sanmit Keni <sanmitkeni@gmail.com>
2022-02-19 14:04:50 +0530

    Task 5

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -4,7 +4,9 @@ import com.pluralsight.blog.data.PostRepository;
 import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -22,8 +24,14 @@ public class BlogController {
         List<Post> allPosts = postRepository.getAllPosts();
         modelMap.put("title", "Blog Post 1");
         modelMap.put("posts", allPosts);
-
         return "home";
+    }
+
+    @RequestMapping("/post/{id}")
+    public String postDetails(@PathVariable Long id, ModelMap modelMap) {
+        Post post = postRepository.findById(id);
+        modelMap.put("post", post);
+        return "post-details";
     }
 
 }
--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -53,6 +53,11 @@ public class PostRepository {
     }
 
     public Post findById(Long id) {
+        for(Post post: ALL_POSTS) {
+            if (post.getId().equals(id)) {
+                return post;
+            }
+        }
         return null;
     }
 }
